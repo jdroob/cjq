786d1e6c8c62470ab28d6b492c30a6b64d810eaf
JohnDRubio <rubiojohn717@gmail.com>
2024-03-15 20:58:28 -0400

    Infrastructure for cjq compiler

---
+++ cjq/main.py
@@ -0,0 +1,10 @@
+import parser
+
+'''
+  This is the entry point to the CJQ compiler.
+'''
+
+if __name__ == '__main__':
+  # TODO: Add in scaffolding for cjq command line args
+  bytecode = None
+  parser.parse(bytecode)
---
+++ cjq/parser.py
@@ -0,0 +1,13 @@
+'''
+  Name pending
+
+  Translate JQ Bytecode to JQ IR.
+'''
+
+def parse(bytecode):
+  '''
+    Generates intermediate representation (JQ IR) from JQ bytecode.
+
+    JQ IR is more amenable for lowering to LLVM IR.
+  '''
+  pass
