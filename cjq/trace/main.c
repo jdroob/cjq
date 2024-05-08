a0fb32a37469f6a29d4a80878eaa1ebba663d9f1
JohnDRubio <rubiojohn717@gmail.com>
2024-05-08 15:55:07 -0400

    Refactor - now able to read in *.jq files instead of only reading filters as raw strings.

--- cjq/trace/main.c
+++ cjq/trace/main.c
@@ -4,7 +4,6 @@
 #include <stdbool.h>
 
 #include "../clib/lib.h"
-
 #include "../jq/src/compile.h"
 #include "../jq/src/jv.h"
 #include "../jq/src/jq.h"
@@ -15,8 +14,8 @@
 #include "cjq_trace.h"
 
 #define PY_SSIZE_T_CLEAN
-#include <../../usr/include/python3.12/Python.h>      // TODO: yuck
-#include <../../usr/include/python3.12/pyconfig.h>      // TODO: yuck
+#include <../../usr/include/python3.12/Python.h>      
+#include <../../usr/include/python3.12/pyconfig.h>   
 
 #define jq_exit(r)              exit( r > 0 ? r : 0 )
 
@@ -28,46 +27,39 @@ void clean_up(compiled_jq_state *cjq_state, trace *opcodes) {
     // cjq_init should never have been called during this phase so cjq_free shouldn't be needed
 }
 
-int get_llvm_ir(compiled_jq_state *cjq_state, trace *opcodes) {
-    // Get value of HOME environment variable
-    char *home = getenv("HOME");
-    if (home == NULL) {
-        fprintf(stderr, "Failed to retrieve HOME environment variable\n");
-        return 1;
-    }
-
-    // Construct full path to the cjq directory
-    char path_to_cjq[256]; 
-    snprintf(path_to_cjq, sizeof(path_to_cjq), "%s/cjq/cjq/trace", home);
-
+int initialize_python(const char *path_to_cjq, PyObject **pModule_llvmlite, PyObject **pModuleLowering) {
     // Initialize CPython
     Py_Initialize();
-
+    
     // Append paths to Python sys.path
     PyRun_SimpleString("import sys");
+    char python_code[512];
+    snprintf(python_code, sizeof(python_code), "sys.path.append(\"%s\")", path_to_cjq);
+    PyRun_SimpleString(python_code);
     PyRun_SimpleString("sys.path.append('/home/rubio/anaconda3/envs/numbaEnv/lib/python3.12/site-packages/')\n");
 
     // Import llvmlite module
-    PyObject *pModule_llvmlite = PyImport_ImportModule("llvmlite");
-    if (!pModule_llvmlite) {
+    *pModule_llvmlite = PyImport_ImportModule("llvmlite");
+    if (!(*pModule_llvmlite)) {
         PyErr_Print();
         fprintf(stderr, "Failed to import module 'llvmlite'\n");
         return 1;
     }
 
-    // Append cjq directory to Python path
-    char python_code[512];
-    snprintf(python_code, sizeof(python_code), "sys.path.append(\"%s\")", path_to_cjq);
-
     // Import lowering module
+    snprintf(python_code, sizeof(python_code), "sys.path.append(\"%s\")", path_to_cjq);
     PyRun_SimpleString(python_code);
-    PyObject *pModuleLowering = PyImport_ImportModule("lowering");
-    if (!pModuleLowering) {
+    *pModuleLowering = PyImport_ImportModule("lowering");
+    if (!(*pModuleLowering)) {
         PyErr_Print();
         fprintf(stderr, "Failed to import module 'lowering'\n");
         return 1;
     }
 
+    return 0;
+}
+
+int get_llvm_ir(compiled_jq_state *cjq_state, trace *opcodes, PyObject *pModule_llvmlite, PyObject *pModuleLowering) {
     // Get jq_lower function from lowering module
     PyObject *pFuncGenerateLLVMIR = PyObject_GetAttrString(pModuleLowering, "generate_llvm_ir");
     if (!pFuncGenerateLLVMIR || !PyCallable_Check(pFuncGenerateLLVMIR)) {
@@ -76,22 +68,13 @@ int get_llvm_ir(compiled_jq_state *cjq_state, trace *opcodes) {
         return 1;
     }
 
-    // compiled_jq_state *pcjq_state = &cjq_state;
-    printf("Actual address (opcodes): %p\n", opcodes);
-    printf("Actual address (cjq_state): %p\n", cjq_state);
-
     // Convert cjq_state pointer to a Python integer object
-    // Create PyObject pointer that points to this Python object
     PyObject *opcodes_ptr = PyLong_FromVoidPtr((void*)opcodes);
     PyObject *cjq_state_ptr = PyLong_FromVoidPtr((void*)cjq_state);
-    // printf("current pc: %i\n", *pcjq_state->pc);
-
-    printf("PyObject address (opcodes_ptr): %p\n",opcodes_ptr);
-    printf("PyObject address (cjq_state_ptr): %p\n",cjq_state_ptr);
 
     // Call generate_llvm_ir
     PyObject *pResult = PyObject_CallFunctionObjArgs(pFuncGenerateLLVMIR, opcodes_ptr, cjq_state_ptr, NULL);
-    printf("Back in cjq/main.c\n");
+
     // Cleanup
     Py_DECREF(pResult);
     Py_DECREF(pFuncGenerateLLVMIR);
@@ -99,11 +82,27 @@ int get_llvm_ir(compiled_jq_state *cjq_state, trace *opcodes) {
     Py_DECREF(pModule_llvmlite);
     Py_Finalize();
 
-    // Return gracefully
     return 0;
 }
 
 int main(int argc, char *argv[]) {
+    char *home = getenv("HOME");
+    if (home == NULL) {
+        fprintf(stderr, "Failed to retrieve HOME environment variable\n");
+        return 1;
+    }
+
+    char path_to_cjq[256]; 
+    snprintf(path_to_cjq, sizeof(path_to_cjq), "%s/cjq/cjq/trace", home);
+
+    PyObject *pModule_llvmlite = NULL;
+    PyObject *pModuleLowering = NULL;
+
+    if (initialize_python(path_to_cjq, &pModule_llvmlite, &pModuleLowering) != 0) {
+        // Error occurred during Python initialization
+        return 1;
+    }
+
     // For storing tracing information
     trace *opcodes = malloc(sizeof(trace));
 
@@ -114,7 +113,7 @@ int main(int argc, char *argv[]) {
     int trace_error = cjq_trace(argc, argv, opcodes);
     
     // Generate LLVM IR
-    int gen_ir_error = get_llvm_ir(cjq_state, opcodes);
+    int gen_ir_error = get_llvm_ir(cjq_state, opcodes, pModule_llvmlite, pModuleLowering);
 
     // Clean up
     clean_up(cjq_state, opcodes);
