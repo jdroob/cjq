147a7f4af8b83568d7ef403dd8cd5d9654b26d5b
JohnDRubio <rubiojohn717@gmail.com>
2024-05-24 15:43:55 -0400

    Formatting

--- cjq/trace/cjq_trace.h
+++ cjq/trace/cjq_trace.h
@@ -14,6 +14,7 @@ typedef struct {
 void trace_init(trace *opcodes, uint8_t *opcode_list, int opcode_list_len, 
                 uint16_t* jq_next_entry_list, int jq_next_entry_list_len,
                 int jq_halt_loc);
+                
 int cjq_trace(int argc, char *argv[], trace *opcodes);
 
 #endif  /* CJQ_TRACE_H */
